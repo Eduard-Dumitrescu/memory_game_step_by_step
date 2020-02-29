6144ab64e933557008224c44c43fe9648e59f95f
Eduard-Dumitrescu <eduard.dumitrescu94@gmail.com>
2020-03-01 01:18:02 +0200

    New Project commit

---
+++ android/app/src/main/kotlin/com/example/memory_game_step_by_step/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.memory_game_step_by_step
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
