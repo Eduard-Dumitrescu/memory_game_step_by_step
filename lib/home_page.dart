cffb925a380d71690598c31e285bfd417bb7d699
Eduard-Dumitrescu <eduard.dumitrescu94@gmail.com>
2020-03-01 02:14:45 +0200

    make grid

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -28,11 +28,29 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Widget _mainBody() {
-    return Board();
+    return Container(
+      //set width and height so that rows and columns will behave
+      width: Utils.deviceWidth(context),
+      height: Utils.deviceHeightWithoutAppBar(context),
+      child: Board(),
+    );
   }
 }
 
 class Board extends StatefulWidget {
+  final Color mainColor;
+  final Color cardColor;
+  final double numOfElements;
+  final double numOfColumns;
+
+  const Board(
+      {Key key,
+      this.mainColor = Colors.green,
+      this.cardColor = Colors.redAccent,
+      this.numOfElements = 16,
+      this.numOfColumns = 4})
+      : super(key: key);
+
   @override
   _BoardState createState() => _BoardState();
 }
@@ -41,10 +59,42 @@ class _BoardState extends State<Board> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      //set width and height so that rows and columns will behave
-      width: Utils.deviceWidth(context),
-      height: Utils.deviceHeightWithoutAppBar(context),
-      color: Colors.green,
+      color: widget.mainColor,
+      child: _gridWidget(),
+    );
+  }
+
+  Widget _gridWidget() {
+    List<Widget> rows = List<Widget>();
+
+    for (int i = 0; i <= widget.numOfElements / widget.numOfColumns; i++) {
+      List<Widget> elements = List<Widget>();
+
+      for (int j = 0; j <= widget.numOfColumns; j++) {
+        elements.add(Flexible(
+          flex: 1,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              color: widget.cardColor,
+            ),
+          ),
+        ));
+      }
+      rows.add(Flexible(
+        flex: 1,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: elements,
+        ),
+      ));
+    }
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: rows,
     );
   }
 }
