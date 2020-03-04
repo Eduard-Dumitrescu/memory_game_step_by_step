427fbc36cf8577c87543011b89a3da750bb61d12
Eduard-Dumitrescu <eduard.dumitrescu94@gmail.com>
2020-03-04 21:07:47 +0200

    ios width / height still needs more research

--- lib/utils.dart
+++ lib/utils.dart
@@ -6,17 +6,20 @@ import 'package:flutter/material.dart';
 class Utils {
   static double deviceWidth(BuildContext context) {
     final double deviceWidth = MediaQuery.of(context).size.width;
-    return Platform.isIOS ? deviceWidth / 2 : deviceWidth;
+    //return Platform.isIOS ? deviceWidth / 2 : deviceWidth;
+    return deviceWidth;
   }
 
   static double deviceHeight(BuildContext context) {
     final double deviceHeight = MediaQuery.of(context).size.height;
-    return Platform.isIOS ? deviceHeight / 2 : deviceHeight;
+    //return Platform.isIOS ? deviceHeight / 2 : deviceHeight;
+    return deviceHeight;
   }
 
   static double deviceHeightWithoutAppBar(BuildContext context) {
     final double deviceHeight =
         MediaQuery.of(context).size.height - kToolbarHeight;
-    return Platform.isIOS ? deviceHeight / 2 : deviceHeight;
+    //return Platform.isIOS ? deviceHeight / 2 : deviceHeight;
+    return deviceHeight;
   }
 }
